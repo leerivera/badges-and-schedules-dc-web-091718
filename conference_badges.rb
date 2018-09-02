a63a94b4f55b5c40f9d2e502a8e898dd52e5feb7
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 02:29:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,8 @@ end
 def batch_badge_creator(array)
   array.collect { |name| "Hello, my name is " + name + "."} 
 end  
-  
+
+def assign_rooms(array) 
+  result = array.each_with_index.map {|item, index| "Hello, #{item}! You'll be assigned to room #{index + 1}!" } 
+  result 
+end 
