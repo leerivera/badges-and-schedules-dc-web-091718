cb6845151c12386e64d607f848baecc86287b4b2
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 02:31:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,16 @@ def assign_rooms(array)
   result = array.each_with_index.map {|item, index| "Hello, #{item}! You'll be assigned to room #{index + 1}!" } 
   result 
 end 
+
+def printer(array)
+  resultOne = batch_badge_creator(array)
+  resultOne.each do |x|
+    puts x 
+  end
+  #loop through first array and print one by one 
+  result = assign_rooms(array)
+  result.each do |x|
+    puts x 
+  end
+  #loop through assign_rooms array and out put those one by one 
+end
