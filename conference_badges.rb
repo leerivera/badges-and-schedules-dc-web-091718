d3ed54ad559fc678176214bc44bb7232195c3be9
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 02:24:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,20 @@
 # Write your code here.
-def badge_maker(array)
+def badge_maker(name)
   
+  name = name.chomp
+ "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(array)
+  attendees.each do |attendees|
+  puts "Hi, my name is #{fruit}."
+ end
+  
+  
+  
+  
+  
+end
+ 
+end
   
